311aa851316262362f066be18851256af55c3940
Jacques Rott <jacques.rott@gmail.com>
2014-11-04 00:13:26 +0100

    Add multi core optimisation

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,46 @@ use std::f64;
 use std::os;
 
 
+fn compute_n_steps(start: uint, end: uint) -> f64 {
+    let mut pi = 0.0_f64;
+    let mut step = if (start - 1) % 4 == 0 {
+        1.0_f64
+    } else {
+        -1.0_f64
+    };
+    for d in range_step_inclusive(start, end, 2) {
+        pi += step / (d as f64);
+        step = -step;
+    }
+    return pi;
+}
+
+fn compute(steps: uint) -> f64 {
+    let mut pi = 0.0_f64;
+    let (sender, receiver): (Sender<f64>, Receiver<f64>) = channel();
+    let cpus = os::num_cpus();
+    let split_steps = steps / cpus;
+
+    for cpu in range(0, cpus) {
+        let child_sender = sender.clone();
+        let start = split_steps * cpu + 1;
+        let end = start + split_steps - 1;
+
+        spawn(proc() {
+            child_sender.send(compute_n_steps(start, end));
+        });
+    }
+    for _ in range(0, cpus) {
+        pi += receiver.recv();
+    }
+    return pi;
+}
+
 fn main() {
     let args: Vec<String> = os::args();
     let opts = [
         optopt("s", "steps", "Number of steps to compute pi [default=1000]", "1000"),
+        optflag("o", "optimize", "Optimise the computation by using multicores"),
         optflag("h", "help", "print this help message and exit"),
     ];
 
@@ -31,12 +67,14 @@ fn main() {
         return;
     }
 
-    let mut pi = 0.0_f64;
-    let mut step = 1.0_f64;
-    for d in range_step_inclusive(1, steps, 2) {
-        pi += step / (d as f64);
-        step = -step;
-    }
+    let optimise = matches.opt_present("o");
+
+    let mut pi = if optimise {
+        compute(steps)
+    } else {
+        compute_n_steps(1, steps)
+    };
+
     pi *= 4.0_f64;
     println!("{}", f64::to_string(pi));
 }
